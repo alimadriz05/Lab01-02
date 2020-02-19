aaa5929413562797a05c158bc88ce29a8b79430e
alimadriz05 <alimadriz05>
2020-02-19 16:21:45 -0600

    jjj

---
+++ src/lab01/pkg02/Lab0102.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01.pkg02;
+
+/**
+ *
+ * @author Allison
+ */
+public class Lab0102 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        ////holaaaapr
+    }
+    
+}
